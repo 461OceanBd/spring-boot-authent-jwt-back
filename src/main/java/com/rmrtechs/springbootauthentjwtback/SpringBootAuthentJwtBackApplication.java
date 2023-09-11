982dbb050ff0a6a98d9d42fb7d99e7a30af80b4d
Romain Mercier <romain.mercier321@gmail.com>
2023-09-11 11:10:09 +0200

    Technique :
    Ajout init du maven project

---
+++ src/main/java/com/rmrtechs/springbootauthentjwtback/SpringBootAuthentJwtBackApplication.java
@@ -0,0 +1,13 @@
+package com.rmrtechs.springbootauthentjwtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthentJwtBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthentJwtBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmrtechs/springbootauthentjwtback/SpringBootAuthentJwtBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmrtechs.springbootauthentjwtback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthentJwtBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
