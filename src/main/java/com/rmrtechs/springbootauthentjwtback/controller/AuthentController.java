a16038aad2675690bfed9de01049dca5e39855c1
Romain Mercier <romain.mercier321@gmail.com>
2023-10-17 14:23:52 +0200

    User :
    Création du controller d'authentification et ajout du endpoint de signup
    d'un user

---
+++ src/main/java/com/rmrtechs/springbootauthentjwtback/controller/AuthentController.java
@@ -0,0 +1,35 @@
+package com.rmrtechs.springbootauthentjwtback.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rmrtechs.springbootauthentjwtback.model.User;
+import com.rmrtechs.springbootauthentjwtback.services.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class AuthentController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/sign-up")
+	public ResponseEntity<?> signupUser(@RequestBody User user) {
+		
+		try {
+			User savedUser = userService.createUser(user);
+			return ResponseEntity.ok(savedUser);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	
+}
