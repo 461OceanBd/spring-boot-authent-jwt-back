8bb62c436844260a00effbae2ff7e9b2c0627eef
Romain Mercier <romain.mercier321@gmail.com>
2023-10-23 17:11:31 +0200

    Authentication :
    - Ajout de l'encodage BCrypt et utilisation dans le controller pour
    enregistrer le mdp hashé

--- src/main/java/com/rmrtechs/springbootauthentjwtback/config/SecurityConfig.java
+++ src/main/java/com/rmrtechs/springbootauthentjwtback/config/SecurityConfig.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -37,15 +36,10 @@ public class SecurityConfig {
      * 
      * @return
      */
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//    	
-//    }
-    
     @Bean
-    public PasswordEncoder passwordEncoder() { // TODO
-		return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
     	
     }
+
 }
--- src/main/java/com/rmrtechs/springbootauthentjwtback/controller/AuthentController.java
+++ src/main/java/com/rmrtechs/springbootauthentjwtback/controller/AuthentController.java
@@ -3,11 +3,14 @@ package com.rmrtechs.springbootauthentjwtback.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rmrtechs.springbootauthentjwtback.dto.UserDto;
+import com.rmrtechs.springbootauthentjwtback.mapper.UserMapper;
 import com.rmrtechs.springbootauthentjwtback.model.User;
 import com.rmrtechs.springbootauthentjwtback.services.UserService;
 
@@ -21,12 +24,21 @@ public class AuthentController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@PostMapping("/sign-up")
-	public ResponseEntity<?> signupUser(@RequestBody User user) {
+	public ResponseEntity<?> signupUser(@RequestBody UserDto userDto) {
 		
 		try {
-			User savedUser = userService.createUser(user); // TODO renvoyer dto sans password et role
-			return ResponseEntity.ok(savedUser);
+			String hashPwd = passwordEncoder.encode(userDto.getPassword());
+			userDto.setPassword(hashPwd);
+			User convertedUser = userMapper.userDtoToUser(userDto);
+			User savedUser = userService.createUser(convertedUser);
+			return ResponseEntity.ok(userMapper.userToUserDtoWithIdAndEmail(savedUser));
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
